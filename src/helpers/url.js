5044f008f1be7fae631a043b05e0b807dbfbc1d4
Sash <tribeca1983@gmail.com>
2022-01-28 12:53:25 +0300

    f

--- src/helpers/url.js
+++ src/helpers/url.js
@@ -1,2 +1,2 @@
-export const url = "https://priceless-hypatia-f0c719.netlify.app/api";
-//export const url = "http://localhost:3004";
+//export const url = "https://priceless-hypatia-f0c719.netlify.app/api";
+export const url = "http://localhost:3004";

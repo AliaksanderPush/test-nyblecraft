62b5f2ac3ce4bf458df9ebe5a892c6ebb16c2152
Sash <tribeca1983@gmail.com>
2022-01-28 09:32:09 +0300

    4

--- src/helpers/url.js
+++ src/helpers/url.js
@@ -1,2 +1,2 @@
-export const urlNotes = "https://priceless-hypatia-f0c719.netlify.app/api/noutes";
-//export const urlNotes = "http://localhost:3004/noutes";
+//export const urlNotes = "https://priceless-hypatia-f0c719.netlify.app/api/noutes";
+export const urlNotes = "http://localhost:3004/noutes";
